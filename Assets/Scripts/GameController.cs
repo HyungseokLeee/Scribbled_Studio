c2ee507c004c4b518488c9cf77b6a25bc1a72119
Philip <hyngsuki@gmail.com>
2020-11-07 19:56:31 -0500

    Added script for start button to change scene to level1 scene and exit button to quit the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject StartButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        switch(SceneManager.GetActiveScene().name)
+        {
+            case ("Main"):
+                Debug.Log("Start scene!");
+                break;
+            case ("Level1Scene"):
+                Debug.Log("Level1 scene!");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("Level1Scene");
+        Debug.Log("Scene is changed to Level 1!");
+    }
+    public void OnExitButtonClick()
+    {
+        Debug.Log("Game is terminated!");
+        Application.Quit();
+    }
+}
